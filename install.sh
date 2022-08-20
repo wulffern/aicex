f0871b6d72d7b09824063ee4f351aa18bbe1aab3
Carsten Wulff <carsten@wulff.no>
2022-08-20 20:36:13 +0200

    Updated symbol reference

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # make sure pip is installed
+    sudo apt install -y python3-pip
+fi
+
 #- Get cIcCreator
 mkdir cic
 cd cic
 git clone https://github.com/wulffern/ciccreator.git
 cd ciccreator
-git branch develop
+git checkout develop
 cd ../../
 
 #- Get cicpy
@@ -20,7 +25,6 @@ cd ..
 git clone https://github.com/wulffern/cicsim.git
 cd cicsim
 git checkout develop
-python3 -m pip install -r requirements.txt --user
 python3 -m pip install --user -e .
 cd ..
 
@@ -29,8 +33,19 @@ git clone https://github.com/wulffern/aicex.git
 cd aicex
 git submodule init
 git submodule update
+cd ..
 
-echo "Trying to compile ciccreator"
-# Try and compile ciccreator
-cd cic/ciccreator
-make
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "You need to install qt5 and compile ciccreator manually"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Trying to compile ciccreator"
+
+    # Try and compile ciccreator
+    cd cic/ciccreator
+
+    # Fresh ubuntu 20 LTS needs
+    sudo apt install -y make qtbase5-dev build-essential --fix-missing
+
+    make
+fi
