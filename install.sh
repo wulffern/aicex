3f4f14800d1a9ab9b5ee37a26e4936f99c368ef8
Carsten Wulff <carsten@wulff.no>
2022-08-20 15:41:22 +0200

    changed to https on clones in install

--- install.sh
+++ install.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
 
+#- Get cIcCreator
 mkdir cic
 cd cic
 git clone https://github.com/wulffern/ciccreator.git
 cd ciccreator
 git branch develop
 cd ../../
+
+#- Get cicpy
 git clone https://github.com/wulffern/cicpy.git
 cd cicpy
 pip3 install -r requirements.txt --user
 pip3 install --user -e .
+cd ..
+
+#- Get cicsim
 git clone https://github.com/wulffern/cicsim.git
 cd cicsim
 git checkout develop
 pip3 install -r requirements.txt --user
 pip3 install --user -e .
 cd ..
+
+#- Get aicex
 git clone https://github.com/wulffern/aicex.git
 cd aicex
 git submodule init
