f396294948844d4b92611a27ce6d3c37850b931e
Carsten Wulff <carsten@wulff.no>
2022-09-28 20:33:00 +0200

    Fixing permissions

--- tests/install_ubuntu.sh
+++ tests/install_ubuntu.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 #
 #- Prerequisites
-apt update
-apt -y install python3-pip libqt5core5a libxft2 libxss1 libgl1-mesa-glx libglu1-mesa libxaw7
+sudo apt update
+sudo apt -y install python3-pip libqt5core5a libxft2 libxss1 libgl1-mesa-glx libglu1-mesa libxaw7
 
 export AICEX=${PWD}
 #- Get EDA
 cd /
-test -d opt || mkdir opt
+sudo test -d opt || mkdir opt
 cd opt
+sudo chmod 777 .
 test -d eda || git clone https://github.com/wulffern/eda
 cd ${AICEX}
