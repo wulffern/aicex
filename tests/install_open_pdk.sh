dc025ff1dcad4bf6b091878e4b28b584a992724e
Carsten Wulff <carsten@wulff.no>
2022-10-13 21:03:58 +0200

    Updated open_pdk install

--- tests/install_open_pdk.sh
+++ tests/install_open_pdk.sh
@@ -1,7 +1,26 @@
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/eda/lib
+export PATH=/opt/eda/bin:$HOME/.local/bin:$PATH
+
+#- Pre install skywater pdk
+sudo mkdir --parents /opt/pdk
+chmod 777 /opt/pdk
+cd /opt/pdk
+git clone https://github.com/google/skywater-pdk
+cd skywater-pdk
+git submodule init libraries/sky130_fd_io/latest
+git submodule init libraries/sky130_fd_pr/latest
+#git submodule init libraries/sky130_fd_sc_hd/latest
+#git submodule init libraries/sky130_fd_sc_hvl/latest
+#Skip most of the libraries, usully not needed for analog design
+git submodule update
+make timing
+cd /opt/pdk
+
+#- Use open pdk to install pdk
 git clone https://github.com/RTimothyEdwards/open_pdks
 cd open_pdks
-./configure --enable-sky130-pdk --prefix=/opt/pdk/ \
+./configure --enable-sky130-pdk=/opt/pdk/skywater-pdk --prefix=/opt/pdk/ \
     --enable-irsim=no \
     --enable-openlane=no \
     --enable-qflow=no \
@@ -12,3 +31,6 @@ cd open_pdks
     --enable-sram-gf180mcu=no
 make
 sudo make install
+
+#- Patch missing metal resistor
+sudo cp /opt/pdk/share/pdk/sky130A/libs.tech/xschem/sky130_fd_pr/res_generic_li.sym  /opt/pdk/share/pdk/sky130A/libs.tech/xschem/sky130_fd_pr/res_generic_l1.sym
