959f72141b4f11718cfa66ca3ac0e4c9d58c7de6
Carsten Wulff <carsten@wulff.no>
2022-10-16 09:10:50 +0200

    typo in install

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/wulffern/aicex.git
 cd aicex
 mkdir .ssh
-cat ~/.ssh/id rsa.pub > .ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub > .ssh/authorized_keys
 docker run --rm  -p 2022:22 -v `pwd`:/home/aicex/ -i wulffern/aicex:latest &
