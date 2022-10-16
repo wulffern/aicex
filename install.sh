49ac930c4ef0f0e5cd0f4569f8221a6f60fe088e
Carsten Wulff <carsten@wulff.no>
2022-10-16 09:09:31 +0200

    Re-added install, now with docker as default

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/wulffern/aicex.git
+cd aicex
+mkdir .ssh
+cat ~/.ssh/id rsa.pub > .ssh/authorized_keys
+docker run --rm  -p 2022:22 -v `pwd`:/home/aicex/ -i wulffern/aicex:latest &
