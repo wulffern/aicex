fefc9b0e47e8d7a903aa255b72bf0282df001077
Carsten Wulff <carsten@wulff.no>
2023-01-22 22:45:16 +0100

    Updated action

--- tests/run_docker_test.sh
+++ tests/run_docker_test.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 /bin/bash tests/install_cicconf.sh
+
+cd ip/cicsim
+python3 -m pip install --user -e .
+cd ../..
+
+cd ip/cicpy/
+python3 -m pip install --user -e .
+cd ../..
+
 /bin/bash tests/run_tests.sh
